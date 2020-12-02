3005a74c73aebd8f33734711eec4c7a457d87cd4
Vaidehi Joshi <vaidehi.sj@gmail.com>
2020-12-01 16:43:38 -0800

    wip

--- app/helpers/authentication_helper.rb
+++ app/helpers/authentication_helper.rb
@@ -30,11 +30,7 @@ module AuthenticationHelper
   end
 
   def forem_creator_flow_enabled?
-    FeatureFlag.enabled?(:creator_onboarding) && waiting_on_first_user?
-  end
-
-  def waiting_on_first_user?
-    SiteConfig.waiting_on_first_user
+    FeatureFlag.enabled?(:creator_onboarding) && SiteConfig.waiting_on_first_user
   end
 
   def invite_only_mode_or_no_enabled_auth_options
